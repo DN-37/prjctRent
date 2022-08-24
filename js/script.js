62f7c090b65826f9c7e8cbe5440b31c35c744f19
Danila Nefedov <dn375@mail.ru>
2022-08-24 18:21:56 +0400

    added swiper for section popular

--- js/script.js
+++ js/script.js
@@ -26,3 +26,21 @@ if (filter) {
         })
     })
 }
+
+const swiper = new Swiper('.popular-slider', {
+    spaceBetween: 20,
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        992: {
+            slidesPerView: 3,
+        },
+        660: {
+            slidesPerView: 2,
+        }
+    }
+  });
